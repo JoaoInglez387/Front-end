42f09d69e750fde07b495b6542aed4b3d1c41234
João Inglez <159451850+JoaoInglez387@users.noreply.github.com>
2024-10-30 08:32:48 -0400

    Add files via upload

---
+++ Aula_03/script.js
@@ -0,0 +1,43 @@
+/* function clique () {
+    console.log('Botão clicado!');
+}
+
+clique() */
+
+const p = document.querySelector('#paragrafo');
+const divImg = document.querySelector('#divImg');
+
+/* divImg.addEventListener('click', () => {
+    divImg.style.border = '2px solid black';
+}) */
+
+function pMouse () {
+    divImg.style.border = '2px solid black';
+}
+
+function sMouse () {
+    divImg.style.border = 'none';
+}
+
+divImg.addEventListener('mouseover', pMouse);
+divImg.addEventListener('mouseout', sMouse); 
+
+
+p.onclick = () => {
+    alert('Paragrafo clicando!.')
+}
+
+const clique = () => {
+    console.log('Botão clicado!');
+    alert('Botão clicado!');
+}
+
+function pressTecla() {
+    if (window.getComputedStyle(divImg, null).getPropertyValue('background-color') === 'rgb(0,0,0)') {
+        divImg.style.backgroundColor = 'rgb(255, 255, 255)';
+    }
+    else {
+        divImg.style.backgroundColor = 'rgb(0,0,0)';
+    }
+}
+
