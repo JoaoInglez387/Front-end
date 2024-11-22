d304a97b9fd7c9a70d81a18db7c3675836bc0b36
JoaoInglez387 <joaoinglez383@gmail.com>
2024-11-22 08:21:48 -0400

    atuli

--- Projetos de Atividades - Front_end_2/React/projeto/src/components/Header/index.js
+++ Projetos de Atividades - Front_end_2/React/projeto/src/components/Header/index.js
@@ -1,16 +1,23 @@
-import styles from "./header.module.css";
+'use client';
+;import styles from "./header.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header () {
+    const [ showMenu, setShowMenu ] = useState(true);
     return (
         <header className = {styles.header}>
-            <nav className = {styles.nav}>
-                <ul className = {styles.list}>
-                    <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/sobre'>Sobre</Link></li>
-                    <li><Link href='/contato'>Contato</Link></li>
-                </ul>
-            </nav>
+            <button className = {styles.button} onClick={() => setShowMenu(!showMenu)}>Clique</button>
+            {
+                showMenu &&
+                <nav className = {styles.nav}>
+                    <ul className = {styles.list}>
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/sobre'>Sobre</Link></li>
+                        <li><Link href='/contato'>Contato</Link></li>
+                    </ul>
+                </nav>
+            }
         </header>
     )
 };
