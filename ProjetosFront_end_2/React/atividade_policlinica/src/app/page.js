8eb7208ab885e2c2e957c9558a80b262f459fe82
João Gabriel Soares Inglez <2023103030048@aluno.ifro.edu.br>
2025-02-13 15:54:09 -0400

    main_novo

--- ProjetosFront_end_2/React/atividade_policlinica/src/app/page.js
+++ ProjetosFront_end_2/React/atividade_policlinica/src/app/page.js
@@ -6,13 +6,14 @@ export default function Home() {
     <main className={styles.main}>
         <section className={styles.contener_banner}>
             <div className={styles.banner}>
-                <Image src="/banner_home.jpg" layout="fill" objectFit="contain" alt="Banner do Home MediVital"/>
+                {/* <Image src="/img/banner_home.jpg" layout="fill" objectFit="contain" alt="Banner do Home MediVital"/> */}
             </div>
         </section>
         <section className={styles.page_contener}>
             <div className={styles.text_inicio}>
                 <h1 className={styles.titulo_text}>Bem-vindo à CLÍNICA MEDVITAL</h1>
                 <p className={styles.conteudo_text}>Onde excelência e cuidado se unem para promover sua saúde. Nossa equipe de especialistas, com o apoio das mais avançadas tecnologias, oferece um atendimento personalizado e preciso. Na MedVital, acreditamos que o cuidado com a saúde vai além do tratamento – é um compromisso contínuo com o seu bem-estar e o de sua família.</p>
+                <button className={styles.StarderButton}>Agendar consulta</button>
             </div>
         </section>
     </main>
--- ProjetosFront_end_2/React/atividade_policlinica/src/components/Header/index.js
+++ ProjetosFront_end_2/React/atividade_policlinica/src/components/Header/index.js
@@ -10,20 +10,37 @@ export default function Header () {
     return (
         <header className = {styles.header}>
             <div className={styles.conteudo_header}>
-                <div className={styles.logo}>
-                    <Image src="/img/logo_MedVital.png" width={150} height={85} alt="Logo Cantinho do Sabor" />
+                <div className={styles.logoContener}>
+                    <Image className={styles.logo} src="/logo_MedVital.png" width={130} height={43} alt="Logo da Clinica MediVital" />
                 </div>
 
                 <nav className={styles.navbar}>
                     <ul className = {styles.nav_itens}>
                         <li className={styles.links_itens}><Link href='/'>Home</Link></li>
                         <li className={styles.links_itens}>Médicos
-                        {/* <ul className={styles.submenu}>
-                            <li>Test</li>
-                        </ul> */}
+                            <ul className={styles.submenu}>
+                                <li className={styles.links_subemenu}><Link href='/'>Listar</Link></li>
+                                <li className={styles.links_subemenu}><Link href='/'>Adicionar</Link></li>
+                                <li className={styles.links_subemenu}><Link href='/'>Editar</Link></li>
+                                <li className={styles.links_subemenu}><Link href='/'>Excluir</Link></li>
+                            </ul>
                         </li>
-                        <li className={styles.links_itens}>Pacientes</li>
-                        <li className={styles.links_itens}>Agendamento</li>
+                        <li className={styles.links_itens}>Pacientes
+                            <ul className={styles.submenu}>
+                                <li className={styles.links_subemenu}><Link href='/'>Listar</Link></li>
+                                <li className={styles.links_subemenu}><Link href='/'>Adicionar</Link></li>
+                                <li className={styles.links_subemenu}><Link href='/'>Editar</Link></li>
+                                <li className={styles.links_subemenu}><Link href='/'>Excluir</Link></li>
+                            </ul>
+                        </li>
+                        <li className={styles.links_itens}>Agendamento
+                            <ul className={`${styles.submenu} ${styles.tamanh_g}`}>
+                                <li className={styles.links_subemenu}><Link href='/'>Listar Consultas</Link></li>
+                                <li className={styles.links_subemenu}><Link href='/'>Agendar Consultas</Link></li>
+                                <li className={styles.links_subemenu}><Link href='/'>Editar Consultas</Link></li>
+                                <li className={styles.links_subemenu}><Link href='/'>Cancelar</Link></li>
+                            </ul>
+                        </li>
                     </ul>
                 </nav>
 
