f3fd4998c7f85ae0bf878eb3ea6ca96d36094079
JoaoInglez387 <joaoinglez383@gmail.com>
2025-02-14 08:40:45 -0400

    novo_altera

--- ProjetosFront_end_2/React/atividade_policlinica/src/app/pages/medicos/listar/page.js
+++ ProjetosFront_end_2/React/atividade_policlinica/src/app/pages/medicos/listar/page.js
@@ -7,15 +7,26 @@ export default function PageListarMedicos() {
             <section className={styles.contener_lista}>
                 <div className={styles.header_list}>
                     <h1 className={styles.titulo_list}>Lista de Médicos</h1>
-                    <button className={styles.StarderButton}>Buscar os médicos</button>
+                    <div className={styles.contener_button}>
+                        <button className={styles.StarderButton}>Buscar os médicos</button>
+                    </div>
                 </div>
                 <div className={styles.table_list}>
-                    <table>
-                        <thead>
-                            <tr key="">
-                                <th></th>
+                    <table className={styles.tabela_geral}>
+                        <thead className={styles.header_table}>
+                            <tr className={styles.conteudo_table}>
+                                <th scope="col" className={styles.colun_table}>ID</th>
+                                <th scope="col" className={styles.colun_table}>Nome</th>
+                                <th scope="col" className={styles.colun_table}>Telefone</th>
+                                <th scope="col" className={styles.colun_table}>Email</th>
+                                <th scope="col" className={styles.colun_table}>Especialidade</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            <tr>
+                                <th></th>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </section>
